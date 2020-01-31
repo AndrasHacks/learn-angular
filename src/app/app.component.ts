2c9b45dfb66607f6d29f9e5d78f234b9ce92c852
András Ács <andras.acs@dealogic.com>
2020-01-31 21:01:47 +0100

    Understanding typescript decorators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+function log(target, name, descriptor) {
+  console.log("Target: ", target, "Name: ", name, "Descriptor: ", descriptor);
+  const original = descriptor.value;
+  descriptor.value = () => {
+    console.log("This function was hacked!");
+  };
+  return descriptor;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'ng-intro';
+  title = "ng-intro";
+
+  constructor() {
+    this.aSimpleMethod();
+  }
+
+  @log
+  aSimpleMethod() {
+    console.log("This is a decorated Typescript function");
+  }
 }
