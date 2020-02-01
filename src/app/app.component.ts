feb9b16fd6d5bf900e8bd3b67918b3b8eefd5f19
András Ács <andras.acs@dealogic.com>
2020-02-01 10:32:34 +0100

    LogCall decorator to log args and result of a call.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from "@angular/core";
 
-function log(target, name, descriptor) {
+function sampleDecorator(target, name, descriptor) {
   console.log("Target: ", target, "Name: ", name, "Descriptor: ", descriptor);
   const original = descriptor.value;
   descriptor.value = () => {
     console.log("This function was hacked!");
+  };
+  return descriptor;
+}
+
+function logCall(target: object, name: string, descriptor: any) {
+  const original: Function = descriptor.value;
+  descriptor.value = function(...args) {
+    console.log(
+      `Log: Function '${name}' was called with `,
+      args,
+      " arguments."
+    );
+    const reuslt = original.apply(this, args);
+    console.log("Log: Result is ", reuslt);
+    return reuslt;
   };
   return descriptor;
 }
@@ -19,9 +34,15 @@ export class AppComponent {
 
   constructor() {
     this.aSimpleMethod();
+    console.log("Result from constructor: ", this.square(5));
   }
 
-  @log
+  @logCall
+  square(a: number) {
+    return a * a;
+  }
+
+  @sampleDecorator
   aSimpleMethod() {
     console.log("This is a decorated Typescript function");
   }
